b4e465d94abff59c1e01d17434b5db6740dd292f
Brianna <briannabarnes1990@gmail.com>
2019-04-26 08:15:00 -0700

    Add files via upload

---
+++ button.js
@@ -0,0 +1,14 @@
+//slider for the images that are being sold on the website
+
+$(document).ready(function(){
+        $('.pictures').slick({
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            fade: true,
+            variableHeight: true,
+            });
+        });    
+
+
+    
